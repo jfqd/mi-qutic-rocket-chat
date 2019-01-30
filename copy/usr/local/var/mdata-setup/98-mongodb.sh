6ec2343011d52e07d84bfd725ca5d7db28bc2492
jfqd <jfqd@blun.org>
2019-01-30 20:24:11 +0100

    add options

---
+++ copy/usr/local/var/mdata-setup/98-mongodb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if mdata-get mongodb_url 1>/dev/null 2>&1; then
+  MONGO_URL=$(mdata-get mongodb_url)
+  gsed -i \
+       -e "s#Environment=MONGO_OPLOG_URL=mongodb://127.0.0.1:27017/rocket#Environment=MONGO_OPLOG_URL={MONGO_URL}#" \
+       -e "s#Environment=MONGO_URL=mongodb://127.0.0.1:27017/rocket#Environment=MONGO_URL={MONGO_URL}#" \
+       /etc/systemd/system/rocketchat.service
+fi
---
+++ copy/usr/local/var/mdata-setup/99-app-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if mdata-get mail_smarthost 1>/dev/null 2>&1; then
+  UID=$(mdata-get mail_auth_user)
+  PWD=$(mdata-get mail_auth_pass)
+  HOST=$(mdata-get mail_smarthost)
+  gsed -i \
+       -e "s#smtp://noreply%40example.com:password@mail.example.com:587#smtp://${UID}:${PWD}@${HOST}#" \
+       /etc/systemd/system/rocketchat.service
+fi
+
+# systemctl daemon-reload
+# systemctl enable rocketchat.service
+# systemctl start rocketchat
+# journalctl -f -u rocketchat

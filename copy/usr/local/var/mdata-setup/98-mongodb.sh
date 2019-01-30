734656eebe3090689a14fed34ff120f5d91d5751
jfqd <jfqd@blun.org>
2019-01-30 22:23:24 +0100

    start mongodb

--- copy/usr/local/var/mdata-setup/98-mongodb.sh
+++ copy/usr/local/var/mdata-setup/98-mongodb.sh
@@ -6,4 +6,7 @@ if mdata-get mongodb_url 1>/dev/null 2>&1; then
        -e "s#Environment=MONGO_OPLOG_URL=mongodb://127.0.0.1:27017/rocket#Environment=MONGO_OPLOG_URL={MONGO_URL}#" \
        -e "s#Environment=MONGO_URL=mongodb://127.0.0.1:27017/rocket#Environment=MONGO_URL={MONGO_URL}#" \
        /etc/systemd/system/rocketchat.service
+  
+  systemctl start mongodb
+  echo 'rs.initiate()' | mongo
 fi

286e010e50492c9ebd5ccf4df0e981a5c9e536e8
jfqd <jfqd@blun.org>
2019-01-31 09:11:49 +0100

    no gsed available, use sed

--- copy/usr/local/var/mdata-setup/includes/98-mongodb.sh
+++ copy/usr/local/var/mdata-setup/includes/98-mongodb.sh
@@ -2,11 +2,11 @@
 
 if /native/usr/sbin/mdata-get mongodb_url 1>/dev/null 2>&1; then
   MONGO_URL=$(/native/usr/sbin/mdata-get mongodb_url)
-  gsed -i \
+  sed -i \
        -e "s#Environment=MONGO_OPLOG_URL=mongodb://127.0.0.1:27017/rocket#Environment=MONGO_OPLOG_URL={MONGO_URL}#" \
        -e "s#Environment=MONGO_URL=mongodb://127.0.0.1:27017/rocket#Environment=MONGO_URL={MONGO_URL}#" \
        /etc/systemd/system/rocketchat.service
-  
+else
   systemctl start mongodb
   echo 'rs.initiate()' | mongo
 fi
--- copy/usr/local/var/mdata-setup/includes/99-app-setup.sh
+++ copy/usr/local/var/mdata-setup/includes/99-app-setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if /native/usr/sbin/mdata-get mail_smarthost 1>/dev/null 2>&1; then
-  UID=$(/native/usr/sbin/mdata-get mail_auth_user)
-  PWD=$(/native/usr/sbin/mdata-get mail_auth_pass)
-  HOST=$(/native/usr/sbin/mdata-get mail_smarthost)
-  gsed -i \
-       -e "s#smtp://noreply%40example.com:password@mail.example.com:587#smtp://${UID}:${PWD}@${HOST}#" \
+  MAIL_UID=$(/native/usr/sbin/mdata-get mail_auth_user)
+  MAIL_PWD=$(/native/usr/sbin/mdata-get mail_auth_pass)
+  MAIL_HOST=$(/native/usr/sbin/mdata-get mail_smarthost)
+  sed -i \
+       -e "s#smtp://noreply%40example.com:password@mail.example.com:587#smtp://${MAIL_UID}:${MAIL_PWD}@${MAIL_HOST}#" \
        /etc/systemd/system/rocketchat.service
 fi
 
 if /native/usr/sbin/mdata-get rocketchat_domain 1>/dev/null 2>&1; then
   RC_DOMAIN=$(/native/usr/sbin/mdata-get rocketchat_domain)
-  gsed -i \
+  sed -i \
        -e "s#ROOT_URL=https://rocket-chat.example.com/#ROOT_URL=${RC_DOMAIN}#" \
        /etc/systemd/system/rocketchat.service
   

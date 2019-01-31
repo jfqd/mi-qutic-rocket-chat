4916d3cdfa243faf40f037685ce8f0419bc8799a
jfqd <jfqd@blun.org>
2019-01-31 08:08:18 +0100

    set full path to mdata-get

--- copy/usr/local/var/mdata-setup/includes/98-mongodb.sh
+++ copy/usr/local/var/mdata-setup/includes/98-mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if mdata-get mongodb_url 1>/dev/null 2>&1; then
-  MONGO_URL=$(mdata-get mongodb_url)
+if /native/usr/sbin/mdata-get mongodb_url 1>/dev/null 2>&1; then
+  MONGO_URL=$(/native/usr/sbin/mdata-get mongodb_url)
   gsed -i \
        -e "s#Environment=MONGO_OPLOG_URL=mongodb://127.0.0.1:27017/rocket#Environment=MONGO_OPLOG_URL={MONGO_URL}#" \
        -e "s#Environment=MONGO_URL=mongodb://127.0.0.1:27017/rocket#Environment=MONGO_URL={MONGO_URL}#" \
--- copy/usr/local/var/mdata-setup/includes/99-app-setup.sh
+++ copy/usr/local/var/mdata-setup/includes/99-app-setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if mdata-get mail_smarthost 1>/dev/null 2>&1; then
-  UID=$(mdata-get mail_auth_user)
-  PWD=$(mdata-get mail_auth_pass)
-  HOST=$(mdata-get mail_smarthost)
+if /native/usr/sbin/mdata-get mail_smarthost 1>/dev/null 2>&1; then
+  UID=$(/native/usr/sbin/mdata-get mail_auth_user)
+  PWD=$(/native/usr/sbin/mdata-get mail_auth_pass)
+  HOST=$(/native/usr/sbin/mdata-get mail_smarthost)
   gsed -i \
        -e "s#smtp://noreply%40example.com:password@mail.example.com:587#smtp://${UID}:${PWD}@${HOST}#" \
        /etc/systemd/system/rocketchat.service
 fi
 
-if mdata-get rocketchat_domain 1>/dev/null 2>&1; then
-  RC_DOMAIN=$(mdata-get rocketchat_domain)
+if /native/usr/sbin/mdata-get rocketchat_domain 1>/dev/null 2>&1; then
+  RC_DOMAIN=$(/native/usr/sbin/mdata-get rocketchat_domain)
   gsed -i \
        -e "s#ROOT_URL=https://rocket-chat.example.com/#ROOT_URL=${RC_DOMAIN}#" \
        /etc/systemd/system/rocketchat.service

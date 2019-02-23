123fcc502225c7186b2044715642220b272c8a13
jfqd <jfqd@blun.org>
2019-02-23 11:46:17 +0100

    fix sed

--- copy/usr/local/var/mdata-setup/includes/98-mongodb.sh
+++ copy/usr/local/var/mdata-setup/includes/98-mongodb.sh
@@ -3,8 +3,7 @@
 if /native/usr/sbin/mdata-get mongodb_url 1>/dev/null 2>&1; then
   MONGO_URL=$(/native/usr/sbin/mdata-get mongodb_url)
   sed -i \
-       -e "s#Environment=MONGO_OPLOG_URL=mongodb://127.0.0.1:27017/rocket#Environment=MONGO_OPLOG_URL={MONGO_URL}#" \
-       -e "s#Environment=MONGO_URL=mongodb://127.0.0.1:27017/rocket#Environment=MONGO_URL={MONGO_URL}#" \
+       -e "s#Environment=MONGO_URL=mongodb://127.0.0.1:27017/rocket#Environment=MONGO_URL=${MONGO_URL}#" \
        /etc/systemd/system/rocketchat.service
 else
   systemctl start mongodb

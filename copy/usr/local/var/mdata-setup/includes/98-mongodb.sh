7ed5ad69d44dc3b1ea7004590bc948e4d1ecc8dd
jfqd <jfqd@blun.org>
2019-03-05 11:08:12 +0100

    fix munin-node restart

--- copy/usr/local/var/mdata-setup/includes/98-mongodb.sh
+++ copy/usr/local/var/mdata-setup/includes/98-mongodb.sh
@@ -6,6 +6,9 @@ if /native/usr/sbin/mdata-get mongodb_url 1>/dev/null 2>&1; then
        -e "s#Environment=MONGO_URL=mongodb://127.0.0.1:27017/rocket#Environment=MONGO_URL=${MONGO_URL}#" \
        /etc/systemd/system/rocketchat.service
   systemctl stop mongodb || true
+  # remove mongo-plugins for munin
+  rm -rf /etc/munin/plugins/mongo_* || true
+  systemctl restart munin-node || true
 else
   systemctl start mongodb || true
   echo 'rs.initiate()' | mongo

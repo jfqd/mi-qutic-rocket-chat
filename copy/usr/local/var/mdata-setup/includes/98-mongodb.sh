dea47a0ea606479971b2ab68bd782127ef7a35b4
jfqd <jfqd@blun.org>
2019-09-27 11:24:35 +0200

    command name is now mongod

--- copy/usr/local/var/mdata-setup/includes/98-mongodb.sh
+++ copy/usr/local/var/mdata-setup/includes/98-mongodb.sh
@@ -5,11 +5,12 @@ if /native/usr/sbin/mdata-get mongodb_url 1>/dev/null 2>&1; then
   sed -i \
        -e "s#Environment=MONGO_URL=mongodb://127.0.0.1:27017/rocket#Environment=MONGO_URL=${MONGO_URL}#" \
        /etc/systemd/system/rocketchat.service
-  systemctl stop mongodb || true
+  systemctl stop mongod || true
   # remove mongo-plugins for munin
   rm -rf /etc/munin/plugins/mongo_* || true
   systemctl restart munin-node || true
 else
-  systemctl start mongodb || true
-  echo 'rs.initiate()' | mongo
+  systemctl enable mongod || true
+  systemctl start mongod || true
+  mongo --eval "printjson(rs.initiate())"
 fi
